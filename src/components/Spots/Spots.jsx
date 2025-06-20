e6704ebf484319bc7764e1eb985ad7237c1f8422
Rvjais <ranveerjais9984@gmail.com>
2025-06-20 07:21:02 +0530

    responsive and added pages

--- src/components/Spots/Spots.jsx
+++ src/components/Spots/Spots.jsx
@@ -1,25 +1,168 @@
 import React from "react";
-import '../Spots/spots.css'
+import "../Spots/spots.css";
 import tour1 from "../../assets/tour1.png";
 import tour2 from "../../assets/tour2.webp";
 import tour3 from "../../assets/tour3.webp";
 import tour4 from "../../assets/tour4.webp";
 import tour5 from "../../assets/tour5.webp";
 import tour6 from "../../assets/tour6.webp";
-import tour7 from "../../assets/tour7.avif";
+import tour7 from "../../assets/tour7.webp";
 import tour8 from "../../assets/tour8.webp";
 
 const Spots = () => {
   return (
     <>
-      <img src={tour1} className="tourimg" alt="" />
-      <img src={tour2} className="tourimg" alt="" />
-      <img src={tour3} className="tourimg" alt="" />
-      <img src={tour4} className="tourimg" alt="" />
-      <img src={tour5} className="tourimg" alt="" />
-      <img src={tour6} className="tourimg" alt="" />
-      <img src={tour7} className="tourimg" alt="" />
-      <img src={tour8} className="tourimg" alt="" />
+      <div className="imgcontainer">
+        <div className="right">
+          <div className="cards">
+            <img src={tour1} className="tourimg" alt="" />
+            <h3 className="name">Name1</h3>
+          </div>
+          <div className="carddescription">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolore quasi aspernatur illo fuga nihil saepe labore, molestiae
+              nisi, id eos iure, dolor dignissimos? Hic voluptatibus est tempora
+              eligendi blanditiis. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quis asperiores nobis nulla ullam tenetur nihil
+              eligendi soluta obcaecati minima in maxime qui totam earum
+              quisquam cum tempore vero, quae eum?{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="left">
+          <div className="cards">
+            <img src={tour2} className="tourimg" alt="" />
+            <h3 className="name">Name1</h3>
+          </div>
+          <div className="carddescription">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolore quasi aspernatur illo fuga nihil saepe labore, molestiae
+              nisi, id eos iure, dolor dignissimos? Hic voluptatibus est tempora
+              eligendi blanditiis. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quis asperiores nobis nulla ullam tenetur nihil
+              eligendi soluta obcaecati minima in maxime qui totam earum
+              quisquam cum tempore vero, quae eum?{" "}
+            </p>
+          </div>
+        </div>
+
+                <div className="right">
+          <div className="cards">
+            <img src={tour3} className="tourimg" alt="" />
+            <h3 className="name">Name1</h3>
+          </div>
+          <div className="carddescription">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolore quasi aspernatur illo fuga nihil saepe labore, molestiae
+              nisi, id eos iure, dolor dignissimos? Hic voluptatibus est tempora
+              eligendi blanditiis. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quis asperiores nobis nulla ullam tenetur nihil
+              eligendi soluta obcaecati minima in maxime qui totam earum
+              quisquam cum tempore vero, quae eum?{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="left">
+          <div className="cards">
+            <img src={tour4} className="tourimg" alt="" />
+            <h3 className="name">Name1</h3>
+          </div>
+          <div className="carddescription">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolore quasi aspernatur illo fuga nihil saepe labore, molestiae
+              nisi, id eos iure, dolor dignissimos? Hic voluptatibus est tempora
+              eligendi blanditiis. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quis asperiores nobis nulla ullam tenetur nihil
+              eligendi soluta obcaecati minima in maxime qui totam earum
+              quisquam cum tempore vero, quae eum?{" "}
+            </p>
+          </div>
+        </div>
+                <div className="right">
+          <div className="cards">
+            <img src={tour8} className="tourimg" alt="" />
+            <h3 className="name">Name1</h3>
+          </div>
+          <div className="carddescription">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolore quasi aspernatur illo fuga nihil saepe labore, molestiae
+              nisi, id eos iure, dolor dignissimos? Hic voluptatibus est tempora
+              eligendi blanditiis. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quis asperiores nobis nulla ullam tenetur nihil
+              eligendi soluta obcaecati minima in maxime qui totam earum
+              quisquam cum tempore vero, quae eum?{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="left">
+          <div className="cards">
+            <img src={tour5} className="tourimg" alt="" />
+            <h3 className="name">Name1</h3>
+          </div>
+          <div className="carddescription">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolore quasi aspernatur illo fuga nihil saepe labore, molestiae
+              nisi, id eos iure, dolor dignissimos? Hic voluptatibus est tempora
+              eligendi blanditiis. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quis asperiores nobis nulla ullam tenetur nihil
+              eligendi soluta obcaecati minima in maxime qui totam earum
+              quisquam cum tempore vero, quae eum?{" "}
+            </p>
+          </div>
+        </div>
+                <div className="right">
+          <div className="cards">
+            <img src={tour6} className="tourimg" alt="" />
+            <h3 className="name">Name1</h3>
+          </div>
+          <div className="carddescription">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolore quasi aspernatur illo fuga nihil saepe labore, molestiae
+              nisi, id eos iure, dolor dignissimos? Hic voluptatibus est tempora
+              eligendi blanditiis. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quis asperiores nobis nulla ullam tenetur nihil
+              eligendi soluta obcaecati minima in maxime qui totam earum
+              quisquam cum tempore vero, quae eum?{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="left">
+          <div className="cards">
+            <img src={tour7} className="tourimg" alt="" />
+            <h3 className="name">Name1</h3>
+          </div>
+          <div className="carddescription">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              dolore quasi aspernatur illo fuga nihil saepe labore, molestiae
+              nisi, id eos iure, dolor dignissimos? Hic voluptatibus est tempora
+              eligendi blanditiis. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quis asperiores nobis nulla ullam tenetur nihil
+              eligendi soluta obcaecati minima in maxime qui totam earum
+              quisquam cum tempore vero, quae eum?{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
